130a63006c0644af5b78f46964b958a6edbe74b1
Jameson Cotter <jameson.cotter@gmail.com>
2019-09-02 20:21:07 -0400

    adding graphQL server, graphQL schema.

---
+++ graphServer.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
--- schema.js
+++ schema.js
@@ -1,6 +1,31 @@
-const GraphQLObjectType = require('graphql');
-//
-// histogram
-// const HistoType = new GraphQLObjectType({
-//     timestamp: null
-// });
+const redis = require('./src/redisClient');
+const { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLInt } = require('graphql');
+const { toPairs } = require('lodash');
+
+const HistoType = new GraphQLObjectType({
+    name: 'Histogram',
+    fields: () => ({
+        keys: { type: GraphQLList(GraphQLInt) },
+        values: { type: GraphQLList(GraphQLInt) }
+    })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    args:{},
+    fields: () => ({
+        histogram: {
+            type: HistoType,
+            resolve: () => redis.histogram().then(data => ({
+                    keys: toPairs(data).map(k => parseInt(k[0], 10)),
+                    values: toPairs(data).map(k => parseInt(k[1], 10))
+                })
+            )
+        }
+    })
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
