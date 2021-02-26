3105beb22f6a2969e539d476ea296e6cd3572fad
Jameson Cotter-Ciambotti <jameson.cotter@gmail.com>
2021-02-25 20:34:08 -0500

    github workflows main.yml

--- scripts/push-report.sh
+++ scripts/push-report.sh
@@ -1,4 +1,4 @@
-set -euo pipefail
+set -eu pipefail
 
 CURR_DIR="$(pwd)"
 TMP_DIR=$(mktemp -d)
@@ -7,6 +7,14 @@ make report
 
 cd $TMP_DIR
 echo "Using temp directory: $TMP_DIR"
+
+git config --global credential.helper store
+
+echo https://jxc454:"${GITHUB_TOKEN}"@github.com"\n" >> ~/.git-credentials
+
+#git config --global user.email jameson.cotter@gmail.com
+git config --global user.name jxc454
+
 git clone https://github.com/jxc454/servehistogram.git
 cd servehistogram
 #git branch -D gh-pages || echo "gh-pages branch doesn't exist!"
